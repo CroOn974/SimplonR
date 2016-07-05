4384512ea732c14bf83748804c1d12abcb4299c3
Elliot Bouvier <elliotbouvier@yahoo.fr>
2016-07-05 13:37:39 +0400

    Rest

---
+++ src/SimplonR/PlateformeBundle/Controller/ApplicantController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Elliot
+ * Date: 04/07/16
+ * Time: 21:21
+ */
+
+namespace SimplonR\PlateformeBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations\RouteResource;
+use FOS\RestBundle\Controller\FOSRestController;
+use SimplonR\PlateformeBundle\Entity\Applicant;
+
+/**
+ * Class ApplicantController
+ * @package SimplonR\PlateformeBundle\Controller
+ * @RouteResource("Applicant")
+ */
+
+class ApplicantController extends FOSRestController
+{
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * /applicants
+     */
+    public function getApplicantsAction()
+    {
+        $applicant = $this->getDoctrine()->getRepository(Applicant::class)
+            ->findAll();
+        $view = $this->view($applicant, 200);
+
+        return $this->handleView($view);
+
+    }
+
+}
